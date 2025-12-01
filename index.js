7689e15f5f36289b8c8da7c2f150d1544695517a
Leonard <leosmart313@gmail.com>
2025-12-01 19:38:30 +0100

    Second commit

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// const passTest = true;
+// let hasDriversLicense = false;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) {
+//     console.log('I can drive!')
+// }
+
+// function logger() {
+//     console.log("Hi I'm a frontend dev");
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apple, pineapple) {
+//     // console.log(apple, pineapple);
+//     const juice = `Juice made with ${apple} apples and ${pineapple} pineapples`;
+//     return juice;
+// }
+
+// const fruitJuice = fruitProcessor(5, 3);
+// console.log(fruitJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 7);
+// console.log(appleOrangeJuice);
+
+// Function declaration
+/*function calcAge1(birthYear) {
+    const age = 2035 - birthYear;
+    return age;
+}
+
+const age1 = calcAge1(2012);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2035 - birthYear;
+}
+
+const age2 = calcAge2 (2005);
+console.log(age2);*/
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2035 - birthYear;
+}
+
+// Arrow function
