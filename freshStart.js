8330e99a04e7a828db537e03f25e896397c3d940
Leonard <leosmart313@gmail.com>
2025-12-03 23:48:07 +0100

    Strength Training

--- freshStart.js
+++ freshStart.js
@@ -1,2 +1,46 @@
 let js = "amazing";
 console.log(4+98+61-19);
+
+// Data types
+/*let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+javaScriptIsFun = "YES!";
+console.log(javaScriptIsFun);*/
+
+// Basic operators
+/*const currentYear = 2025;
+const mySisterAge = currentYear - 2006;
+const myAge = currentYear - 2003;
+console.log(myAge, mySisterAge);*/
+
+// Comparison Operator
+/*console.log(myAge > mySisterAge);
+console.log(mySisterAge > myAge);
+console.log(myAge >= mySisterAge);*/
+
+// Operator precedence
+/*const currentYear = 2025;
+const mySisterAge = currentYear - 2006;
+const myAge = currentYear - 2003;
+console.log(currentYear - 2006 > currentYear - 2003)*/
+
+/*let iHaveNoIdeaWhatImDoing = "Hello world!";
+// alert(iHaveNoIdeaWhatImDoing);
+console.log(iHaveNoIdeaWhatImDoing);
+
+iHaveNoIdeaWhatImDoing = 7 * 4;
+console.log(iHaveNoIdeaWhatImDoing);*/
+
+/*let sample1 = "Hello👋";
+let sampleTwo = "I'm Leo, I'm an E-rank developer";
+// alert(sample1+""+sampleTwo);
+console.log(sample1+""+sampleTwo);*/
+
+let calc = (4 * 14) + 9;
+console.log(calc);
+
+const skillLevel = 5;
+const skillFourYrs = 1000;
+console.log(skillLevel > skillFourYrs);
+console.log(skillFourYrs > skillLevel);
+console.log(typeof skillLevel);
