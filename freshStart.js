301f2217db5097307682002a084157c7638ee82e
Leonard <leosmart313@gmail.com>
2025-12-08 00:33:32 +0100

    The if else statement

--- freshStart.js
+++ freshStart.js
@@ -46,7 +46,7 @@ console.log(skillFourYrs > skillLevel);
 console.log(typeof skillLevel);*/
 
 // Test 1
-const markWeight = 78;
+/*const markWeight = 78;
 const markHeight = 1.69;
 const johnWeight = 92;
 const johnHeight = 1.95;
@@ -67,4 +67,38 @@ const BMIone = marksWeight/(marksHeight * marksHeight);
 const BMItwo = johnsWeight/(johnsHeight * johnsHeight);
 const BMI = BMIone > BMItwo;
 console.log(BMIone, BMItwo);
-console.log(BMI);
+console.log(BMI);*/
+
+// Strings and template literals
+/*const myName = "Leonard";
+const job = "Novice spellcaster";
+const detail = "I'm " + myName + ", a " + job+ "!";
+console.log(detail);
+// template literals
+const newDetails = `I'm ${myName}, a ${job}!`;
+console.log(newDetails);
+
+const Test = `I'm just 
+testing out
+somethings...nice 👍`;
+console.log(Test);*/
+
+// Making decision
+const canDrive = 15;
+const notDrive = 17;
+const age = canDrive > notDrive;
+
+if (age) {
+    console.log(`Sarah is old enough to drive!`);
+} else {
+    console.log(`Nora is not old enough to drive!`)
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
