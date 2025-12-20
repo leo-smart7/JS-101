a555be1aa07d69191d522ac56224ef13f00c0489
Leonard <leosmart313@gmail.com>
2025-12-20 20:17:31 +0100

    Functions

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
 'use strict';
+let hasDriversLicence = false;
+const passTest = true;
 
-// const passTest = true;
-// let hasDriversLicense = false;
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log(`I can drive!`);
 
-// if (passTest) hasDriversLicense = true;
-// if (hasDriversLicense) {
-//     console.log('I can drive!')
-// }
+// Functions
+function logger() {
+    console.log(`Introduction into functions in JavaScript!`);
+}
+logger();
+logger();
+logger();
+logger();
 
-// function logger() {
-//     console.log("Hi I'm a frontend dev");
-// }
-
-// logger();
-// logger();
-// logger();
-
-// function fruitProcessor(apple, pineapple) {
-//     // console.log(apple, pineapple);
-//     const juice = `Juice made with ${apple} apples and ${pineapple} pineapples`;
-//     return juice;
-// }
-
-// const fruitJuice = fruitProcessor(5, 3);
-// console.log(fruitJuice);
-
-// const appleOrangeJuice = fruitProcessor(2, 7);
-// console.log(appleOrangeJuice);
-
-// Function declaration
-/*function calcAge1(birthYear) {
-    const age = 2035 - birthYear;
-    return age;
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples, and ${oranges} oranges`;
+    return juice;
 }
 
-const age1 = calcAge1(2012);
-console.log(age1);
+const fruitMaker = fruitProcessor(4, 0);
+console.log(fruitMaker);
+// console.log(fruitProcessor(6, 8));
+// console.log(fruitProcessor(7, 2));
+// console.log(fruitProcessor(4, 0));
 
-// Function expression
-const calcAge2 = function (birthYear) {
-    return 2035 - birthYear;
-}
-
-const age2 = calcAge2 (2005);
-console.log(age2);*/
-
-// Function expression
-const calcAge2 = function (birthYear) {
-    return 2035 - birthYear;
-}
-
-// Arrow function
+function fruitBlender(watermelon, grape)
