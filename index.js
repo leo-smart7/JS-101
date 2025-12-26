9061eace5f63d4870ac8ca960aa81d6906214dfd
Leonard <leosmart313@gmail.com>
2025-12-26 01:25:30 +0100

    Test practice

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-let hasDriversLicence = false;
+/*let hasDriversLicence = false;
 const passTest = true;
 
 if (passTest) hasDriversLicence = true;
@@ -23,10 +23,10 @@ const fruitMaker = fruitProcessor(4, 0);
 console.log(fruitMaker);
 // console.log(fruitProcessor(6, 8));
 // console.log(fruitProcessor(7, 2));
-// console.log(fruitProcessor(4, 0));
+// console.log(fruitProcessor(4, 0));*/
 
 // Self Practice on Functions
-function fruit(apples, oranges) {
+/*function fruit(apples, oranges) {
     const fruitJuice = `I made fruit juice with ${apples} apples, and ${oranges} oranges!`;
     return fruitJuice;
 }
@@ -39,4 +39,34 @@ function anime(demonslayer, naruto) {
     return animeOtaku;
 }
 
-console.log(anime('demon slayer', 'Naruto'));
+console.log(anime('demon slayer', 'Naruto'));*/
+
+// Function declartion
+/*function calcAge1(birthyear) {
+    return 2037 - birthyear;
+}
+const age1 = calcAge1(1985);
+
+// Function expression
+const calcAge2 = function (birthyear) {
+    return 2037 - birthyear;
+}
+const age2 = calcAge2(1985);
+console.log(age2, age1);*/
+
+// Arrow functions
+const calcAge3 = birthyear => 2037 - birthyear;
+const age3 = calcAge3(1985);
+console.log(age3);
+
+const yearsUntilRetirement = (birthyear, firstname) => {
+    const age = 2037 - birthyear;
+    const retirement = 67 - age;
+    // return retirement;
+    return `${firstname} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1985, 'Mark'));
+console.log(yearsUntilRetirement(1980, 'James'));
+
+// Functions calling other functions
